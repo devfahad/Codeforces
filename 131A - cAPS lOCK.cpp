2760f811a12335ad75059d492fa20d744238d20c
Saidul Haque Fahad <fahadahmed9051@gmail.com>
2016-09-09 21:52:06 +0600

    Update 131A - cAPS lOCK.cpp

--- 131A - cAPS lOCK.cpp
+++ 131A - cAPS lOCK.cpp
@@ -5,30 +5,32 @@ int main(){
 	
 	string s;
 	cin >> s;
-	int len=s.size(),count=0;
-	if(len==1){
-		s[0]=toupper(s[0]);
-		cout << s[0] << endl;
-	}
+	int count=0,len;
+	len = s.size();
 	
-	for(int i = 0; i < len; i++){
-		if(toupper(s[i])) count++;
+	for(int i = 0; i < len ; i++){
+		if(s[i] >='A' && s[i] <= 'Z' ) count++;
 	}
+	//cout << count;
 	if(count == len){
-		for(int i = 0; i < len ; i++) s[i]=tolower(s[i]);
-	}
-	//cout << s;
-	if(s[0] >= 97){
-		for(int i = 1; i < len ; i++){
-			if(toupper(s[i])) count++;
+		for(int i = 0; i < len; i++){
+			s[i]=tolower(s[i]);
+			cout << s[i];
 		}
-		
+		cout << endl;
 	}
-	if(count = len - 1){
-		cout << tolower(s[0]);
-		for(int i = 1; i < len ; i++) cout << tolower(s[i]);
+	else if(count == len-1 && (s[0] >= 'a' && s[0] <= 'z')){\
+		s[0]=toupper(s[0]);
+		cout << s[0];
+		for(int i = 1; i < len; i++){
+			s[i]=tolower(s[i]);
+			cout << s[i];
+		}
+		cout << endl;
 	}
-	
-	cout << endl;
+	else{
+		for(int i = 0; i < len ; i++) cout << s[i];
+		cout << endl;
+	}
 	return 0;
 }
